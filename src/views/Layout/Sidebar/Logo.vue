7d6550020e751626b39ca69a498ab9313914992a
colinw <colinw@synnex.com>
2019-01-25 21:00:56 +0800

    添加logo

--- src/views/Layout/Sidebar/Logo.vue
+++ src/views/Layout/Sidebar/Logo.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="logo">
-    <i class="el-icon-edit"></i>
-    <span>xiuery</span>
+    <li>
+      <icon-x icon-class="setting"/>
+      <span>xiuery</span>
+    </li>
   </div>
 </template>
 
@@ -11,9 +13,27 @@ export default {
 }
 </script>
 
-<style>
+<style rel="stylesheet/scss" lang="scss" scoped>
   .logo {
+    width: 100%;
     height: 50px;
+    line-height: 50px;
+    color: rgb(191, 203, 217);
     background-color: rgb(67, 82, 100);
+    li {
+      width: 100%;
+      font-size: 14px;
+      padding: 0 20px;
+      cursor: pointer;
+      overflow: hidden;
+      display: inline-block;
+      svg {
+        width: 24px;
+        font-size: 18px;
+        margin-right: 5px;
+        text-align: center;
+        vertical-align: middle;
+      }
+    }
   }
 </style>
